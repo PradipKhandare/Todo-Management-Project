1bb8dc4924c1d71fef94cbcaa8db8a17ccaf2c44
PradipKhandare <pradip.khandare@neutrinotechlabs.com>
2024-08-20 19:00:48 +0530

    Initial commit

---
+++ src/main/java/com/learn/springboot/todo_managemnt/TodoManagemntApplication.java
@@ -0,0 +1,13 @@
+package com.learn.springboot.todo_managemnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoManagemntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoManagemntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/springboot/todo_managemnt/TodoManagemntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.springboot.todo_managemnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoManagemntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
